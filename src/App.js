fd90330891718d7aa363b38310df6b3d30036eb7
JangHoonSeok <orirori512@naver.com>
2022-09-03 16:26:01 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+const animation = keyframes`
+  0%{
+  transform: rotate(0deg);
+  border-radius: 0px;
+  }
+  50%{
+  transform: rotate(360deg);
+  border-radius: 100px;
+  }
+  100%{
+  transform: rotate(0deg);
+  border-radius: 0px;
+  }
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
