938a8e78a328284d74b2a5ac56192f053c13a77c
JangHoonSeok <orirori512@naver.com>
2022-09-07 15:53:15 +0900

    interface&props

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,9 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const darkTheme = {
-  //다크모드
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-const lightTheme = {
-  //라이트 모드
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
