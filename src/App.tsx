1269c54e5ed0eae31734d6d0ec8e26e336290578
JangHoonSeok <orirori512@naver.com>
2022-09-23 12:43:52 +0900

    Forms-TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 function App() {
   const [value, setValue] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
   };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           value={value}
           onChange={onChange}
