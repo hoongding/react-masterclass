63a102ccb3f845d67f690bfb41a9152d84bfbb3c
JangHoonSeok <orirori512@naver.com>
2022-10-13 17:34:44 +0900

    Recap section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import styled from "styled-components";
+import { BooleanLiteral } from "typescript";
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.bgColor};
@@ -7,10 +9,20 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  otherThingHere?: boolean;
+}
+
+function Dummy({ text, otherThingHere }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
   return (
     <Container>
-      <H1>protected</H1>
+      <Dummy text="hello" />
     </Container>
   );
 }
