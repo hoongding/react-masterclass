5f53494f61f5224c5bc3f1733c3588b2848141ad
JangHoonSeok <orirori512@naver.com>
2022-10-13 17:05:51 +0900

    Theme section

---
+++ src/styled.d.ts
@@ -0,0 +1,11 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string; // 이 곳이 내 테마가 어떻게 보일지를 설명할 부분.
+    bgColor: string;
+    btnColor: string;
+  }
+}
