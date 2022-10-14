c73949560741a74fe502a55c8d84b08bfe6a54d5
JangHoonSeok <orirori512@naver.com>
2022-10-14 17:22:09 +0900

    set up for crypto tracker

--- src/Circle.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-function Circle() {
-  return <div></div>;
-}
-
-export default Circle;
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -6,6 +6,6 @@ declare module "styled-components" {
   export interface DefaultTheme {
     textColor: string; // 이 곳이 내 테마가 어떻게 보일지를 설명할 부분.
     bgColor: string;
-    btnColor: string;
+    accentColor: string;
   }
 }
