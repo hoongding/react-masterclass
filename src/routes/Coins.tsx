3700e9a05a8272e02753fe45b59382758a428e20
JangHoonSeok <orirori512@naver.com>
2022-10-15 19:22:42 +0900

    Home part

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,83 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px; // 이런식으로 하면 모바일 화면처럼 늘여도 가운데에 위치하게된다.
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block; //이렇게 block 을 추가해주면 그 box 끝에 클릭해도 link 발동된다.
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
-function Coin() {
-  return <Title>Coin</Title>;
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })(); // 이런식으로 쓰면 바로 함수 실행가능. 일종의 trick임
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coin</Title>
+      </Header>
+      {loading ? ( // loading이 true면 Loading... 이고 faluse면 뒤에!
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 export default Coin;
